389c4b3cd89ee15fab295144068847d62de18328
Tomas Mokry <tomas.mokry@gmail.com>
2026-09-07 07:43:54 +0200

    Security layer

--- src/main/java/org/tomo/beton/config/SecurityConfig.java
+++ src/main/java/org/tomo/beton/config/SecurityConfig.java
@@ -70,17 +70,9 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(
-                List.of("https://beton-pos.vercel.app")
-        );
-
-        configuration.setAllowedMethods(
-                List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")
-        );
-
-        configuration.setAllowedHeaders(
-                List.of("*")
-        );
+        configuration.setAllowedOrigins(List.of("https://beton-pos.vercel.app"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
 
         configuration.setAllowCredentials(true);
 
